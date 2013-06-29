9ec1dd234669509115281c622d9bec875893a8e6
John van der Wal <johntvanderwal@gmail.com>
2013-06-29 15:13:17 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gomb::Application.config.secret_token = '37ad9fe5608274bd2d72b0b81d0109a4de5cdc8c5cc0da0670728366c6fd078b026de0513e80753e93b416949469d44e884274701fbd8b80b957dd078e3e370f'
